079ae3294f0699543b32fa430c606277d7e9e806
Brad Bell <brad.bb.bell@gmail.com>
2024-08-12 15:59:48 -0600

    logs user in and sets token in local storage

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,38 @@
 <template>
-  <h1 class="text-center text-2xl">Playbook</h1>
+  <h1 class="text-center text-2xl">Playbook for {{ username }}</h1>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    login() {
+      axios
+        .post('https://playbook-api-be781a173fb8.herokuapp.com/api/v1/auth/login/', {
+          email: 'brad.bb.bell@gmail.com',
+          password: 'passwordbb'
+        })
+        .then((response) => {
+          this.username = response.data.username
+          const token = response.data.token
+          localStorage.setItem('token', token)
+          if (token) {
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+          }
+        })
+    },
+    logout() {
+      localStorage.removeItem('token')
+      axios.defaults.headers.common['Authorization'] = ''
+    }
+  },
+  mounted() {
+    this.login()
+  }
+}
+</script>
