48873ad9599c0141f736e4ec550c56e510f1390c
Brad Bell <brad.bb.bell@gmail.com>
2024-08-13 08:35:54 -0600

    adjusts some basics

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
-  <h1 class="text-center text-2xl">Playbook for {{ username }}</h1>
+  <main>
+    <h1 class="text-center text-2xl">Playbook</h1>
+    <div v-for="bet in allBets" :key="bet.id">
+      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2">Title</label>
+          <p class="text-gray-700 text-base">{{ bet.sport }}</p>
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2">Description</label>
+          <p class="text-gray-700 text-base">{{ bet.description }}</p>
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2">Odds</label>
+          <p class="text-gray-700 text-base">{{ bet.odds }}</p>
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2">Status</label>
+          <p class="text-gray-700 text-base">{{ bet.status }}</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 <script>
 import axios from 'axios'
@@ -7,13 +29,18 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      username: ''
+      allBets: []
     }
   },
   methods: {
+    getAllBets() {
+      axios.get('https://playbook-api-399674c1bec2.herokuapp.com/api/v1/bets/').then((response) => {
+        this.allBets = response.data.bets
+      })
+    },
     login() {
       axios
-        .post('https://playbook-api-be781a173fb8.herokuapp.com/api/v1/auth/login/', {
+        .post('https://playbook-api-399674c1bec2.herokuapp.com/api/v1/auth/login/', {
           email: 'brad.bb.bell@gmail.com',
           password: 'passwordbb'
         })
@@ -32,7 +59,7 @@ export default {
     }
   },
   mounted() {
-    this.login()
+    this.getAllBets()
   }
 }
 </script>
